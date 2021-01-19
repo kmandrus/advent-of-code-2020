f25da84a274eaa4e0a61f7d6d61104c1d7155afd
Kevin Andrus <kevin.andrus@gmail.com>
2021-01-19 12:35:06 -0500

    Add refactorings for day 10, likely final

--- day_10/adapter_array.rb
+++ day_10/adapter_array.rb
@@ -4,28 +4,18 @@ class Adapter_Array
     attr_reader :adapters
 
     def initialize(filepath)
-        @adapter_ratings = load_adapter_ratings(filepath)
-        @num_adapter_combos_for_rating = { 0 => 1 }
+        @adapters = load_adapter_ratings(filepath)
     end
 
-    def load_adapter_ratings(filepath)
-        adapter_ratings = File.readlines(filepath, chomp: true)
-            .map(&:to_i)
-            .sort
-        add_charger_and_device_ratings!(adapter_ratings)
-    end
-
-    def add_charger_and_device_ratings!(adapter_ratings)
-        adapter_ratings
-            .unshift(0)
-            .push(adapter_ratings.last + 3)
+    def part_1_answer
+        jolt_jump_count[1] * jolt_jump_count[3]
     end
 
     def jolt_jump_count
         return @jolt_jump_count if @jolt_jump_count
         
         @jolt_jump_count = Hash.new(0)
-        (0...@adapter_ratings.length).each do |i|
+        (0...adapters.length).each do |i|
             count_jolt_difference!(@jolt_jump_count, i)
         end
         @jolt_jump_count
@@ -33,43 +23,66 @@ class Adapter_Array
 
     def count_jolt_difference!(count, adapter_rating_index)
         return count if adapter_rating_index.zero?
-        jolt_rating = @adapter_ratings[adapter_rating_index]
-        prev_jolt_rating = @adapter_ratings[adapter_rating_index - 1]
+        jolt_rating = @adapters[adapter_rating_index]
+        prev_jolt_rating = @adapters[adapter_rating_index - 1]
         difference = jolt_rating - prev_jolt_rating
         count[difference] += 1
     end
 
-    def part_1_answer
-        jolt_jump_count[1] * jolt_jump_count[3]
-    end
-
     def part_2_answer
-        answer = @num_adapter_combos_for_rating[@adapter_ratings.last]
-        return answer if answer 
-
-        ratings = @adapter_ratings
-        ratings.each_with_index do |rating, i|
-            connections = valid_connections(i)
-            sum = connections
-                .sum do |connection|
-                    @num_adapter_combos_for_rating[connection]
-                end
-            @num_adapter_combos_for_rating[rating] = sum unless rating.zero?
-        end
-        @num_adapter_combos_for_rating[@adapter_ratings.last]
+        ways_to_reach_charger(@adapters.last)
     end
 
-    def valid_connections(adapter_ratings_index)
-        i = adapter_ratings_index - 1
-        connections = []
-        return connections if i < 0
-        until @adapter_ratings[i] + 3 < @adapter_ratings[adapter_ratings_index] || i < 0
-            connections << @adapter_ratings[i]
-            i -= 1
+    def ways_to_reach_charger(adapter)
+        @ways_to_reach_charger = { 0 => 1 } unless @ways_to_reach_charger
+        ways = @ways_to_reach_charger[adapter]
+        return ways if ways
+        
+        connections = valid_connections(adapter)
+        ways = connections.sum do |connection| 
+            ways_to_reach_charger(connection)
         end
-        connections
+        @ways_to_reach_charger[adapter] = ways 
     end
 
+    def valid_connections(adapter)
+        adapter_index = to_index(adapter)
+        return [] if (adapter_index - 1).negative?
+        
+        lower_slice_index = adapter_index - 3
+        lower_slice_index = 0 if lower_slice_index.negative?
+        potential_connection_indices = (lower_slice_index...adapter_index)
+
+        @adapters[potential_connection_indices]
+            .select { |connection| connection + 3 >= adapter }
+    end
+
+    def load_adapter_ratings(filepath)
+        adapters = File.readlines(filepath, chomp: true)
+            .map(&:to_i)
+            .sort
+        add_charger_and_device!(adapters)
+    end
+
+    def add_charger_and_device!(adapters)
+        adapters
+            .unshift(0)
+            .push(adapters.last + 3)
+    end
+
+    def to_index(adapter)
+        return @adapter_to_index_hash[adapter] if @adapter_to_index_hash
+
+        @adapter_to_index_hash = Hash.new
+        @adapters.each_with_index do |an_adapter, i|
+            @adapter_to_index_hash[an_adapter] = i
+        end
+        @adapter_to_index_hash[adapter]
+    end
+
+    def is_charger?(adapter)
+        adapter.zero?
+    end
 
 end
 
