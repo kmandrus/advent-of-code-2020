cec4a4b6c2016ec57db6fd429e6a5993ddae7e6e
Kevin Andrus <kevin.andrus@gmail.com>
2021-01-19 10:33:51 -0500

    Refactor day_10

--- day_10/adapter_array.rb
+++ day_10/adapter_array.rb
@@ -4,13 +4,18 @@ class Adapter_Array
     attr_reader :adapters
 
     def initialize(filepath)
-        @adapters = load_adapters(filepath)
+        @adapter_ratings = load_adapter_ratings(filepath)
+        @num_adapter_combos_for_rating = { 0 => 1 }
     end
 
-    def load_adapters(filepath)
-        File.readlines(filepath, chomp: true)
+    def load_adapter_ratings(filepath)
+        adapter_ratings = File.readlines(filepath, chomp: true)
             .map(&:to_i)
             .sort
+
+        adapter_ratings
+            .unshift(0)
+            .push(adapter_ratings.last + 3)
     end
 
     def jolt_jump_log
@@ -18,7 +23,7 @@ class Adapter_Array
         
         @jolt_jump_log = Hash.new(0)
         prev_jolt_rating = 0
-        @adapters.each do |jolt_rating|
+        @adapter_ratings.each do |jolt_rating|
             difference = jolt_rating - prev_jolt_rating
             raise "jolt rating difference is greater than 3" if difference > 3
             @jolt_jump_log[difference] += 1
@@ -29,28 +34,34 @@ class Adapter_Array
     end
 
     def part_1_answer
-        jolt_jump_log[1] * (jolt_jump_log[3] + 1)
+        jolt_jump_log[1] * (jolt_jump_log[3])
     end
 
     def part_2_answer
-        @adapters.unshift(0)
-        @adapter_arrangements = {}
-        @adapters.each_with_index do |rating, i|
-            #put into helpter or think of something better
-            bot = i - 3
-            bot = 0 if bot < 0 
-            #another helper probably
-            connections = @adapters[bot...i]
-                .select { |connection| connection + 3 >= rating }
+        answer = @num_adapter_combos_for_rating[@adapter_ratings.last]
+        return answer if answer 
+
+        ratings = @adapter_ratings
+        ratings.each_with_index do |rating, i|
+            connections = valid_connections(i)
             sum = connections
-                .sum { |connection| @adapter_arrangements[connection] }
-            if rating == 0
-                @adapter_arrangements[rating] = 1
-            else
-                @adapter_arrangements[rating] = sum
-            end
+                .sum do |connection|
+                    @num_adapter_combos_for_rating[connection]
+                end
+            @num_adapter_combos_for_rating[rating] = sum unless rating.zero?
         end
-        @adapter_arrangements[@adapters.last]
+        @num_adapter_combos_for_rating[@adapter_ratings.last]
+    end
+
+    def valid_connections(adapter_ratings_index)
+        i = adapter_ratings_index - 1
+        connections = []
+        return connections if i < 0
+        until @adapter_ratings[i] + 3 < @adapter_ratings[adapter_ratings_index] || i < 0
+            connections << @adapter_ratings[i]
+            i -= 1
+        end
+        connections
     end
 
 
@@ -61,8 +72,10 @@ puts aa.part_1_answer
 puts aa.part_2_answer
 
 #testing
-# test_aa = Adapter_Array.new("test_input.txt")
-# puts test_aa.part_2_answer == 7
+test_aa = Adapter_Array.new("test_input.txt")
+puts test_aa.part_2_answer == 7
+puts aa.part_1_answer == 2380
+puts aa.part_2_answer == 48358655787008
 
 
 
