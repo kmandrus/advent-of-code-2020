99bfcad4ea6770b0272a2c1ce000fb101a198ee9
Kevin Andrus <kevin.andrus@gmail.com>
2021-01-25 16:09:07 -0500

    Incorporate Code Review SE feedback

--- day_10/adapter_array.rb
+++ day_10/adapter_array.rb
@@ -1,64 +1,66 @@
 require "byebug"
 
-class Adapter_Array
+class AdapterArray
+    attr_reader :adapters
 
-    def initialize(filepath)
-        @adapters = load_adapters(filepath)
+    def initialize(adapters)
+        @adapters = adapters.sort
+        add_charger_and_device!(@adapters)
     end
 
-    def load_adapters(filepath)
-        adapters = File.readlines(filepath, chomp: true)
-            .map(&:to_i)
-            .sort
-        add_charger_and_device(adapters)
-    end
-
-    def add_charger_and_device(adapters)
-        adapters.dup
-            .unshift(0)
-            .push(adapters.last + 3)
-    end
-
-    def part_1_answer
+    def jolt_diffs_of_one_times_jolt_diffs_of_three
         jolt_differences[1] * jolt_differences[3]
     end
 
-    def jolt_differences
-        return @count if @count
-
-        @count = Hash.new(0)
-        @adapters.each_with_index do |adapter, i|
-            next if is_charger?(adapter)
-            prev_adapter = @adapters[i-1]
-            @count[adapter - prev_adapter] += 1
-        end
-        @count
-    end
-
-    def part_2_answer
+    def all_possible_charger_arrangements
         ways_to_reach_charger(@adapters.last)
     end
 
+    private
+    CHARGER_RATING = 0
+    def add_charger_and_device!(adapters)
+        adapters
+            .unshift(CHARGER_RATING)
+            .push(device_rating)
+    end
+
+    def jolt_differences
+        @jolt_differences ||= fetch_jolt_differences
+    end
+
+    def fetch_jolt_differences
+        Hash.new(0).tap do |result|
+            adapters.each_with_index do |adapter, i|
+                next if is_charger?(adapter)
+
+                prev_adapter = @adapters[i-1]
+                result[adapter - prev_adapter] += 1
+            end
+        end
+    end
+
     def ways_to_reach_charger(adapter)
-        @memorized_ways = { 0 => 1 } unless @memorized_ways
-        return @memorized_ways[adapter] if @memorized_ways[adapter]
+        @ways_to_reach_charger ||= { 0 => 1 }
+        if @ways_to_reach_charger[adapter]
+            return @ways_to_reach_charger[adapter]
+        end
         
-        @memorized_ways[adapter] = lower_jolt_connections(adapter)
+        @ways_to_reach_charger[adapter] = lower_jolt_connections(adapter)
             .sum { |connection| ways_to_reach_charger(connection) }
     end
 
     def lower_jolt_connections(adapter)
         return [] if is_charger?(adapter)
 
-        connections = []
         i = (to_index(adapter) - 1)
-        potential_connection = @adapters[i]
-        while valid_connection?(adapter, potential_connection) && i >= 0
-            connections << potential_connection
-            i -= 1
-            potential_connection = @adapters[i]
+        [].tap do |connections|
+            potential_connection = adapters[i]
+            while valid_connection?(adapter, potential_connection) && i >= 0
+                connections << potential_connection
+                i -= 1
+                potential_connection = adapters[i]
+            end
         end
-        connections
     end
 
     def valid_connection?(adapter_1, adapter_2)
@@ -75,21 +77,12 @@ class Adapter_Array
         @adapter_to_index_hash[adapter]
     end
 
+    def device_rating
+        adapters.last + 3
+    end
+
     def is_charger?(adapter)
         adapter.zero?
     end
 
 end
-
-aa = Adapter_Array.new("input.txt")
-puts aa.part_1_answer
-puts aa.part_2_answer
-
-#testing
-test_aa = Adapter_Array.new("test_input.txt")
-puts test_aa.part_2_answer == 7
-puts aa.part_1_answer == 2380
-puts aa.part_2_answer == 48358655787008
-
-
-
