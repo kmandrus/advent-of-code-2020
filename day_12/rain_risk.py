97dadaa07f6cab5fabe8da791f8cd73e4252ff72
Kevin Andrus <kevin.andrus@gmail.com>
2021-02-02 11:10:47 -0500

    Refactor inherited RouteFinder methods into a mixin

--- day_12/rain_risk.py
+++ day_12/rain_risk.py
@@ -12,11 +12,35 @@ def load_input(filepath):
     with open(filepath) as file:
         return file.readlines() 
 
-class RouteFinder:
+class OceanTravelMixin:
     north, south, east, west, forward = 'N', 'S', 'E', 'W', 'F'
     right, left = 'R', 'L'
     headings = [north, east, south, west]
-    diffs_by_heading = { north:(0, 1), south:(0, -1), east:(1,0), west:(-1,0) }
+    diffs_by_heading = {
+        north: (0, 1), 
+        south: (0, -1), 
+        east: (1, 0), 
+        west: (-1, 0)
+    }
+
+    def compute_manhattan_distance(self, start, end):
+        delta_x, delta_y = self.subtract_positions(start, end)
+        return abs(delta_x) + abs(delta_y)
+    
+    def add_positions(self, pos_1, pos_2):
+        return (pos_1[0] + pos_2[0], pos_1[1] + pos_2[1])
+    
+    def subtract_positions(self, minuend, subtrahend):
+        return (minuend[0] - subtrahend[0], minuend[1] - subtrahend[1])
+
+    def parse_step(self, str):
+        return (str[:1], int(str[1:]))
+    
+    def compute_move_tuple(self, heading, distance):
+        diff = self.diffs_by_heading[heading]
+        return (diff[0] * distance, diff[1] * distance)
+
+class RouteFinder(OceanTravelMixin):
     
     def __init__(self, route):
         self.start = (0, 0)
@@ -24,11 +48,6 @@ class RouteFinder:
         self.heading = self.east
         self.pos = self.start
         self.travel_route()
-    
-    def compute_manhattan_distance(self):
-        delta_x = abs(self.pos[0] - self.start[0])
-        delta_y = abs(self.pos[1] - self.start[1])
-        return delta_x + delta_y
 
     def travel_route(self):
         for step in self.route:
@@ -43,64 +62,59 @@ class RouteFinder:
             move_tuple = self.compute_move_tuple(self.heading, distance)
         else:
             move_tuple = self.compute_move_tuple(heading, distance)
-        self.pos = self.add(self.pos, move_tuple)
+        self.pos = self.add_positions(self.pos, move_tuple)
 
     def rotate(self, turn_direction, degrees):
-        turn_amount = degrees // 90
+        quarter_turns = degrees // 90
         if turn_direction == self.left:
-            turn_amount *= -1
+            quarter_turns *= -1
         heading_idx = self.headings.index(self.heading)
-        self.heading = self.headings[(heading_idx + turn_amount) % 4]
-    
-    def compute_move_tuple(self, heading, distance):
-        diff = self.diffs_by_heading[heading]
-        return (diff[0] * distance, diff[1] * distance)
-    
-    def add(self, pos_1, pos_2):
-        return (pos_1[0] + pos_2[0], pos_1[1] + pos_2[1])
+        self.heading = self.headings[(heading_idx + quarter_turns) % 4]
 
-    def parse_step(self, str):
-        return (str[:1], int(str[1:]))
-
-class WaypointRouteFinder(RouteFinder):
+class WaypointRouteFinder(OceanTravelMixin):
     rotation_diffs_by_axis = { 0:(1,0), 1:(0,1), 2:(-1,0), 3:(0,-1) }
 
     def __init__(self, route, waypoint=(10, 1)):
         self.waypoint = waypoint
-        super().__init__(route)
+        self.route = route
+        self.start = (0,0)
+        self.pos = self.start
+        self.travel_route()
     
     def move(self, times):
         for i in range(times):
-            self.pos = self.add(self.pos, self.waypoint)
+            self.pos = self.add_positions(self.pos, self.waypoint)
 
-    def rotate(self, turn_direction, degrees):
+    def rotate_waypoint_around_origin(self, turn_direction, degrees):
         num_turns = degrees // 90
         new_waypoint = self.waypoint
         for _ in range(num_turns):
             if turn_direction == self.right:
-                new_waypoint = self.rotate_right_quarter_turn(new_waypoint)
+                new_waypoint = self.rotate_pos_around_origin_90deg_right(
+                    new_waypoint)
             else:
-                new_waypoint = self.rotate_left_quarter_turn(new_waypoint)
+                new_waypoint = self.rotate_pos_around_origin_90deg_left(
+                    new_waypoint)
         self.waypoint = new_waypoint
     
     def move_waypoint(self, heading, distance):
         move_tuple = self.compute_move_tuple(heading, distance)
-        self.waypoint = self.add(self.waypoint, move_tuple)
+        self.waypoint = self.add_positions(self.waypoint, move_tuple)
 
     def travel_route(self):
         for step in self.route:
             cmd, arg = self.parse_step(step)
             if cmd == self.right or cmd == self.left:
-                self.rotate(cmd, arg)
+                self.rotate_waypoint_around_origin(cmd, arg)
             elif cmd == self.forward:
                 self.move(arg)
             else:
                 self.move_waypoint(cmd, arg)
     
-    def rotate_right_quarter_turn(self, pos):
+    def rotate_pos_around_origin_90deg_right(self, pos):
         x, y = pos
         return (y, -x)
     
-    def rotate_left_quarter_turn(self, pos):
+    def rotate_pos_around_origin_90deg_left(self, pos):
         x, y = pos
         return (-y, x)
--- day_12/tests.py
+++ day_12/tests.py
@@ -6,9 +6,15 @@ route_finder = rain_risk.RouteFinder(route)
 wp_route_finder = rain_risk.WaypointRouteFinder(route)
 
 print("part one answer:")
-print(route_finder.compute_manhattan_distance())
+print(route_finder.compute_manhattan_distance(
+    route_finder.start,
+    route_finder.pos
+))
 print("part two answer:")
-print(wp_route_finder.compute_manhattan_distance())
+print(wp_route_finder.compute_manhattan_distance(
+    route_finder.start,
+    route_finder.pos
+))
 
 print("-" * 40)
 print("TESTS - ROUTE FINDER")
@@ -25,9 +31,9 @@ print(test_route_finder.heading == test_route_finder.west)
 test_route_finder.rotate('R', 180)
 print(test_route_finder.heading == test_route_finder.east)
 
-print("#add")
-print(test_route_finder.add((5, 4), (-1, 6)) == (4, 10))
-print(test_route_finder.add((0, 0), (-3, 5)) == (-3, 5))
+print("#add_positions")
+print(test_route_finder.add_positions((5, 4), (-1, 6)) == (4, 10))
+print(test_route_finder.add_positions((0, 0), (-3, 5)) == (-3, 5))
 
 print('#parse_step')
 print(test_route_finder.parse_step('R180') == ('R', 180))
@@ -47,7 +53,10 @@ print("#travel_route")
 print(test_route_finder.pos == (17, -8))
 
 print("#compute_manhattan_distance")
-print(test_route_finder.compute_manhattan_distance() == 25)
+test_route_finder = rain_risk.RouteFinder(test_route)
+print(test_route_finder.compute_manhattan_distance(
+    test_route_finder.start, 
+    test_route_finder.pos) == 25)
 
 print("-" * 40)
 print("TESTS - WAYPOINT ROUTE FINDER")
@@ -71,30 +80,30 @@ print(test_wp_route_finder.waypoint == (-13, -6))
 test_wp_route_finder.move_waypoint('N', 3)
 print(test_wp_route_finder.waypoint == (-13, -3))
 
-print("#rotate")
+print("#rotate_waypoint_around_origin")
 test_wp_route_finder = rain_risk.WaypointRouteFinder([])
 
-test_wp_route_finder.rotate('R', 360)
+test_wp_route_finder.rotate_waypoint_around_origin('R', 360)
 print(test_wp_route_finder.waypoint == (10, 1))
-test_wp_route_finder.rotate('L', 360)
+test_wp_route_finder.rotate_waypoint_around_origin('L', 360)
 print(test_wp_route_finder.waypoint == (10, 1))
-test_wp_route_finder.rotate('R', 90)
+test_wp_route_finder.rotate_waypoint_around_origin('R', 90)
 print(test_wp_route_finder.waypoint == (1, -10))
-test_wp_route_finder.rotate('L', 180)
+test_wp_route_finder.rotate_waypoint_around_origin('L', 180)
 print(test_wp_route_finder.waypoint == (-1, 10))
 
 test_wp_route_finder.waypoint = (-3, 0)
-test_wp_route_finder.rotate('R', 360)
+test_wp_route_finder.rotate_waypoint_around_origin('R', 360)
 print(test_wp_route_finder.waypoint == (-3, 0))
-test_wp_route_finder.rotate('L', 360)
+test_wp_route_finder.rotate_waypoint_around_origin('L', 360)
 print(test_wp_route_finder.waypoint == (-3, 0))
-test_wp_route_finder.rotate('R', 90)
+test_wp_route_finder.rotate_waypoint_around_origin('R', 90)
 print(test_wp_route_finder.waypoint == (0, 3))
-test_wp_route_finder.rotate('L', 180)
+test_wp_route_finder.rotate_waypoint_around_origin('L', 180)
 print(test_wp_route_finder.waypoint == (0, -3))
 
 test_wp_route_finder.waypoint = (0, 0)
-test_wp_route_finder.rotate('R', 90)
+test_wp_route_finder.rotate_waypoint_around_origin('R', 90)
 print(test_wp_route_finder.waypoint == (0,0))
 
 print("#travel_route")
